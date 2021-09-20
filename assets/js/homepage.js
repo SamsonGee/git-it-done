b6afda75ff1e1e516eecf6b609d583ece5d5d771
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-09-20 12:59:23 -0500

    i don't really get the error part

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,9 +9,17 @@ var getUserRepos = function(user) {
 
   // make a request to the url
   fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
+    if (response.ok) {
+      response.json().then(function(data) {
         displayRepos(data, user);
-    });
+      });
+    } else {
+      alert("Error: GitHub User Not Found");
+    }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   });
 };
 
@@ -34,6 +42,12 @@ var displayRepos = function(repos, searchTerm) {
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
+
+    // check if api returned any repos
+    if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+    }
     // loop over repos
     for (var i = 0; i < repos.length; i++) {
     // format repo name
