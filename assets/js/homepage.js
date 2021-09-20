f686320b409812a06c88b567af2f82212c10ff14
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-09-20 05:24:43 -0500

    updating code

---
+++ assets/js/homepage.js
@@ -0,0 +1,7 @@
+var response = fetch("https://api.github.com/users/octocat/repos")
+.then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+      });
+});
+    
